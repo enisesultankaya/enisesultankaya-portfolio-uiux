79c78012144a4abbc543746ec75a9595cc60bde1
Enise Sultan Kaya <109233701+enisesultankaya@users.noreply.github.com>
2025-11-30 00:25:57 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@
 
   export default defineConfig({
     plugins: [react()],
+    base: '/enisesultankaya-portfolio-uiux/',
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
       alias: {
